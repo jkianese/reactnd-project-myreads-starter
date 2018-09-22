cfb9d81778fec36cdd8cdf544b2f723cbce69ea5
[jkianese] <[jkianese@gmail.com]>
2018-09-22 17:22:41 -0400

    Rubric Now, Pretty Later

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import SearchPage from './SearchPage';
-import MainPage from './MainPage';
-import * as BooksAPI from './BooksAPI';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import BookShelf from './BookShelf'
+import SearchBooks from './SearchBooks'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+class BooksApp extends Component {
   state = {
-     books: []
-   } 
+    books: []
+  } 
 
-   componentDidMount() {
-     BooksAPI.getAll().then((books) => { 
-       this.setState ({ books }) // or, books: books
-     })
-   }
-
-   moveToShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf); 
-
-    BooksAPI.getAll().then((books) => { // try to write code that won't duplicate
-      this.setState ({ books }) // or, books: books
+  //get books on load
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => { 
+      this.setState ({books}) 
     })
-   }
-
-  render() {
-    return (
-      <div className="app">
-      <Route exact path="/" render={() => (
-        <MainPage 
-        books={this.state.books}
-        moveToShelf={this.moveToShelf}
-      />
-      )} />
-    
-      <Route path="/search" render={() => (
-        <SearchPage 
-        books={this.state.books}
-        moveToShelf={this.moveToShelf}
-      />  
-      )} />
-      </div> 
-    )
   }
+ 
+ updateBook = (book, shelf) => {
+  BooksAPI.update(book, shelf).then(data =>{
+    book.shelf = shelf
+
+    let newBooks = this.state.books.filter(b => b.id !== book.id)
+
+    newBooks.push(book);
+    this.setState({books: newBooks})
+  })
+}
+
+ render() {
+
+  const { books } = this.state
+
+   return (
+     <div className="app">
+     <Route exact path="/" render={() => (
+       <BookShelf 
+       books={books}
+       updateBook={this.updateBook}
+     />
+     )} />
+
+     {/* <Route path="/search" component={SearchBooks} /> */}
+   
+     <Route path="/search" render={() => (
+       <SearchBooks 
+       books={books}
+       updateBook={this.updateBook}
+     />  
+     )} />
+     </div> 
+   )
+ }
 }
 
 export default BooksApp
--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Book extends Component {
+
     render () {
         
-        let imgThumb = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''; 
+        let coverImage = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : '';  
 
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193,
-                        backgroundImage: `url("${this.props.book.imageLinks.thumbnail}"` }}></div>
+                        backgroundImage: `url("${coverImage}"` }}></div>
                     <div className="book-shelf-changer">
-                        <select onChange={(e) => this.props.moveToShelf(
+
+                        <select onChange={(e) => this.props.updateBook(
                             this.props.book, e.target.value
                         )} 
-                        value={this.props.currentShelf}
+                            value={this.props.currentShelf}
                         >
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
@@ -22,13 +24,18 @@ class Book extends Component {
                             <option value="read">Read</option>
                             <option value="none">None</option>
                         </select>
+
                     </div>
                     </div>
-                <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                    <div className="book-title">
+                        {this.props.book.title}
+                    </div>
+                    <div className="book-authors">
+                        {this.props.book.authors ? this.props.book.authors.toString() : ' '}
+                    </div>
              </div>
         );
     }
 }
 
-export default Book;
+export default Book
